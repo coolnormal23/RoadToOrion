94b02f47dca3bef9dfeedba6b3f913d69e6be12f
coolnormal23 <50341105+coolnormal23@users.noreply.github.com>
2023-01-12 15:44:46 -0600

    property accessor change

--- public/src/script/firebase.js
+++ public/src/script/firebase.js
@@ -53,7 +53,7 @@ document.getElementById("signinbutton").onclick = function() {
       console.log(keys);
       for(var i = 0; i < keys.length; i++)
       {
-        if(data.(keys[i]) == true)
+        if(data[keys[i]] == true)
         {
           var fieldobj = document.getElementById(keys[i]);
           fieldobj.classList.remove('camobutton');
